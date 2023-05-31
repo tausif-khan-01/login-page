db1963d6a86529ea30ef34646b6567610465b3e0
Tausif Khan <khantausifkhan123@gmail.com>
2023-05-31 09:49:36 +0530

    some changes

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 
 const body = document.querySelector("body"),
-      modeToggle = body.querySelector(".mode-toggle");
+    //   modeToggle = body.querySelector(".mode-toggle");
       sidebar = body.querySelector("nav");
       sidebarToggle = body.querySelector(".sidebar-toggle");
+      navItems = body.querySelector(".nav-links")
+      
 
-let getMode = localStorage.getItem("mode");
-if(getMode && getMode ==="dark"){
-    body.classList.toggle("dark");
-}
+// let getMode = localStorage.getItem("mode");
+// if(getMode && getMode ==="dark"){
+//     body.classList.toggle("dark");
+// }
 
 let getStatus = localStorage.getItem("status");
 if(getStatus && getStatus ==="close"){
     sidebar.classList.toggle("close");
 }
 
-modeToggle.addEventListener("click", () =>{
-    body.classList.toggle("dark");
-    if(body.classList.contains("dark")){
-        localStorage.setItem("mode", "dark");
-    }else{
-        localStorage.setItem("mode", "light");
-    }
-});
+
 
 sidebarToggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
@@ -31,3 +26,4 @@ sidebarToggle.addEventListener("click", () => {
         localStorage.setItem("status", "open");
     }
 })
+
